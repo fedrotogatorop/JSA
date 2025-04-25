192d6255b3b15932977ede2b72ae9b98c41a0d7e
Fedro Togatorop <fedrotoga@gmail.com>
2025-04-25 20:11:28 +0700

    single thread

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 console.log(
   "Halo, ini adalah hari pertama saya belajar javascript asynchronous"
 );
+setTimeout(() => {
+  console.log(
+    "ini udah masuk memory proses tapi dijalankan setelah 3 detik kedepan"
+  );
+}, 3000);
+console.log("bakal muncul ke dua");
